77cf6fd8441f8207c972bdf6e5e6f122255e028b
Jenn R <ada_wit11@hackademy.mx>
2022-08-13 02:05:51 -0500

    Creating the CRUD
    Methods to Postman

--- lugares_seguros/lugares_seguros/settings.py
+++ lugares_seguros/lugares_seguros/settings.py
@@ -128,9 +128,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
+# REST_FRAMEWORK = {
+#     'DEFAULT_RENDERER_CLASSES': [
+#         'rest_framework.renderers.JSONRenderer',
         
-    ]
-}
+#     ]
+# }
---
+++ lugares_seguros/places/urls.py
@@ -0,0 +1,9 @@
+#TO CREATE ENDPOINT
+
+from django.urls import path
+from places import views
+
+urlpatterns = [
+    path('', views.PlaceView.as_view()),
+    path('<int:id>', views.PlaceSingleView.as_view()),
+]
